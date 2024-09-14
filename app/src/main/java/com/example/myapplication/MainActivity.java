d33ad3a886f6156a934716d5f56754918a6c53e3
lamfein <lamfein520@gmail.com>
2024-09-14 15:43:11 +0800

    add button in MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName("com.example.myapplication", "com.example.myapplication.SplashActivity"));
+            startActivity(intent);
+        });
+
     }
-    //推出到后台则直接finsh
-    @Override
-    protected void onStop() {
-        super.onStop();
-        finish();
-    }
+
 }
